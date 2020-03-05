28824cce07addd980e70f6eaecadcee967420e1e
M0M0NGA <maxime.rmgx@gmail.com>
2020-03-05 09:45:18 +0100

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h2>Liste de fichiers</h2>
+    <ul>
+        <li>sdsdsd</li>
+        <li>{{nomFichier}}</li>
+    </ul>
+</div>
+</template>
+<script>
+export default {
+    name: 'todos',
+    data(){
+        return{
+            nomFichier: null
+        }
+    },
+    mounted(){
+        this.$http.get("https://czrnafchb7.execute-api.eu-west-3.amazonaws.com/mrcfa/")
+        .then(response => this.nomFichier = response.data.body)
+    }
+}
+</script>
+<style scoped>
+
+</style>
