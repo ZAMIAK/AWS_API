d1352f73e6fb42e10a84aff1d55d668d56802886
Warokk <theo.uzan@hotmail.fr>
2020-03-09 12:02:14 +0100

    list

--- src/App.vue
+++ src/App.vue
@@ -57,57 +57,27 @@
       </v-btn>
     </v-toolbar>
 
-    <v-list two-line subheader>
-      <v-subheader inset>Folders</v-subheader>
-
-      <v-list-item
-        v-for="item in items"
-        :key="item.title" 
-      >
-        <v-list-item-avatar>
-          <v-icon
-            :class="[item.iconClass]"
-            v-text="item.icon"
-          ></v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-
+    <v-list subheader>
       <v-subheader inset>Files</v-subheader>
 
       <v-list-item
         v-for="item in nomFichier"
-        :key="item.data"
+        :key="item.id"
       >
         <v-list-item-avatar>
-          <v-icon
-            :class="[item.iconClass]"
-            v-text="item.icon"
-          ></v-icon>
+           <v-icon color="blue darken-2">mdi-message-text</v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+          <v-list-item-title v-text="item"></v-list-item-title>
+          <!-- <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle> -->
         </v-list-item-content>
 
-        <v-list-item-action>
+        <!-- <v-list-item-action>
           <v-btn icon>
             <v-icon color="grey lighten-1">mdi-information</v-icon>
           </v-btn>
-        </v-list-item-action>
+        </v-list-item-action> -->
       </v-list-item>
     </v-list>
   </v-card>
@@ -132,9 +102,14 @@ export default {
             drawer: null
         }
     },
+    methods:{
+      async getAllfiles(){
+        let response = await this.$http.get("https://czrnafchb7.execute-api.eu-west-3.amazonaws.com/mrcfa/")
+        this.nomFichier = JSON.parse(response.data.body)
+      }
+    },
     mounted(){
-        this.$http.get("https://czrnafchb7.execute-api.eu-west-3.amazonaws.com/mrcfa/")
-        .then(response => this.nomFichier = response.data.body)
+        this.getAllfiles()
     }
 }
 </script>
